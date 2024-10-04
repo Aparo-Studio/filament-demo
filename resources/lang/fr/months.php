d59d2c5a43d25f1ce6dd3d795bc32ce174f3b328
ELIOTOUT Lilian <eliotout.lilian.al@gmail.com>
2024-10-04 15:54:53 +0200

    update: translations files also added inside resources folder

---
+++ resources/lang/fr/months.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'jan' => 'Jan',
+    'feb' => 'Fév',
+    'mar' => 'Mar',
+    'apr' => 'Avr',
+    'may' => 'Mai',
+    'jun' => 'Juin',
+    'jul' => 'Juil',
+    'aug' => 'Août',
+    'sep' => 'Sep',
+    'oct' => 'Oct',
+    'nov' => 'Nov',
+    'dec' => 'Déc',
+];
+
